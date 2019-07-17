0a39a5f37f0428dc83b7717dc92379ea34b15819
Parijat Chauhan <parijat.chauhan@gmail.com>
2019-07-17 21:18:49 +0530

    added promisify

--- index.js
+++ index.js
@@ -39,8 +39,29 @@ function debounce(f, ms) {
     }
 }
 
+// promisify(f, true) to get array of results
+function promisify(f, manyArgs = false) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      function callback(err, ...results) { // our custom callback for f
+        if (err) {
+          return reject(err);
+        } else {
+          // resolve with all callback results if manyArgs is specified
+          resolve(manyArgs ? results : results[0]);
+        }
+      }
+
+      args.push(callback);
+
+      f.call(this, ...args);
+    });
+  };
+};
+
 
 module.exports = {
     throttle,
-    debounce
+    debounce,
+    promisify
 }
